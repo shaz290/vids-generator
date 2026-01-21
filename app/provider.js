7d3d47cb72bcd105fc5c51fc807ab849691c4d3a
shaz.ahmed <shaz.ahmed@KAN-QALAP101>
2026-01-21 14:59:45 +0530

    font and usercreation

--- app/provider.js
+++ app/provider.js
@@ -1,9 +1,34 @@
-import React from 'react'
+"use client"
 
-function Provider({children}) {
+import { db } from '@/configs/db';
+import { Users } from '@/configs/schema';
+import { useUser } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm';
+import React, { useEffect } from 'react'
+
+function Provider({ children }) {
+
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && isNewUser()
+  }, [user]);
+
+  const isNewUser = async () => {
+    const result = await db.select().from(Users)
+      .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+    if (!result[0]) {
+      await db.insert(Users).values({
+        name: user.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        imageUrl: user?.imageUrl
+      })
+    }
+  }
   return (
     <div>
-        {children}
+      {children}
     </div>
   )
 }
